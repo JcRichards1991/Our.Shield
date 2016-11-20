e41b09b363c758cb2d4b5acf3682ec3f8eadd73e
Joshua Richards <jcrichards1991@live.co.uk>
2016-11-20 22:57:49 +0000

    Updated Package action
    issue within the code though

--- src/Shield/Helpers/IPAddressAndDomainRestrictionsHelper.cs
+++ src/Shield/Helpers/IPAddressAndDomainRestrictionsHelper.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shield.IIS.Helpers
+namespace Shield.Helpers
 {
     public class IPAddressAndDomainRestrictionsHelper
     {
