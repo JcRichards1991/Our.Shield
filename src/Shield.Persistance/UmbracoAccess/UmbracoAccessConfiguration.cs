1e5eddb925e5e0021d421d2d899a8bb11da11e09
Joshua Richards <jcrichards1991@live.co.uk>
2016-12-03 19:25:56 +0000

    missed committing file

---
+++ src/Shield.Persistance/UmbracoAccess/UmbracoAccessConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Shield.Persistance.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shield.Persistance.UmbracoAccess
+{
+    public class UmbracoAccessConfiguration : IConfiguration
+    {
+        public string Name
+        {
+            get
+            {
+                return "UmbracoAccess";
+            }
+        }
+
+        private string settings;
+        public string Settings
+        {
+            get
+            {
+                return settings;
+            }
+
+            set
+            {
+                IsDirty = true;
+                settings = value;
+            }
+        }
+
+        public bool IsDirty { get; set; }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConfiguration Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
