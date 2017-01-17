042478bc16c5984332d8aa568ad872a0e5521479
Joshua Richards <jcrichards1991@live.co.uk>
2017-01-17 23:34:50 +0000

    updated post build events

---
+++ src/Shield.UI/Initialise/app.js
@@ -0,0 +1,22 @@
+﻿angular.module('shield', ['umbraco', 'shield.resources']);
+angular.module('shield.resources', []);
+
+angular.module('umbraco').directive("ngIsolateApp", function () {
+    return {
+        "scope": {},
+        "restrict": "AEC",
+        "compile": function (element, attrs) {
+            var html = element.html();
+            element.html('');
+            return function (scope, element) {
+                scope.$destroy();
+                setTimeout(function () {
+                    var newRoot = document.createElement("div");
+                    newRoot.innerHTML = html;
+                    angular.bootstrap(newRoot, [attrs["ngIsolateApp"]]);
+                    element.append(newRoot);
+                });
+            }
+        }
+    }
+});
