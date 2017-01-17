9c633c6655a3a5598d54b681b207400c06cab4b4
Joshua Richards <jcrichards1991@live.co.uk>
2017-01-17 23:54:40 +0000

    clean up of packages.json & gulpfile.js

--- src/Shield.UI/gulpfile.js
+++ src/Shield.UI/gulpfile.js
@@ -4,25 +4,9 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var gutil = require('gulp-util');
 var rename = require("gulp-rename");
-var map = require("map-stream");
-var livereload = require("gulp-livereload");
 var concat = require("gulp-concat");
 var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
-var minify = require('gulp-minify');
-var merge = require('merge-stream');
 var insert = require('gulp-insert');
-
-global.errorMessage = '';
-
-var sassFiles = [
-	//{
-	//	watch: '_scss/Site/**/*.scss',
-	//	sass: '_scss/Site/site.scss',
-	//	output: './www/app/View/Themed/Site/webroot/css',
-	//	name: 'site.css',
-	//}
-];
 
 var jsFiles = [
     'Initialise/app.js',
@@ -30,31 +14,7 @@ var jsFiles = [
 ];
 
 var jsOutput = 'Scripts/';
-//END configuration
 
-
-//gulp.task('watch', function () {
-//    for (var i in sassFiles) {
-//    	sassWatch(sassFiles[i]);
-//    }
-//});
-
-//function sassWatch(sassData) {
-//    gulp.src(sassData.watch)
-//	.pipe(watch({ glob: sassData.watch, emitOnGlob: true }, function () {
-//	    gulp.src(sassData.sass)
-//		.pipe(sass(sassOptions))
-//		.on('error', function (err) {
-//		    gutil.log(err.message);
-//		    gutil.beep();
-//		    global.errorMessage = err.message + " ";
-//		})
-//		.pipe(checkErrors())
-//		.pipe(rename(sassData.name))
-//		.pipe(gulp.dest(sassData.output))
-//		.pipe(livereload());
-//	}));
-//}
 
 gulp.task('default', ['buildJS']);
 
@@ -67,27 +27,3 @@ gulp.task('buildJS', function () {
     .pipe(rename('Shield.min.js'))
     .pipe(gulp.dest(jsOutput));
 });
-
-var sassOptions = {
-    'style': 'compressed',
-    'unixNewlines': true,
-    'cacheLocation': '_scss/.sass_cache'
-};
-
-var checkErrors = function (obj) {
-    function checkErrors(file, callback, errorMessage) {
-        if (file.path.indexOf('.scss') != -1) {
-            file.contents = new Buffer("\
-					body * { white-space:pre; }\
-					body * { display: none!important; }\
-					body:before {\
-						white-space:pre;\
-						content: '"+ global.errorMessage.replace(/(\\)/gm, "/").replace(/(\r\n|\n|\r)/gm, "\\A") + "';\
-					}\
-					html{background:#ccf!important; }\
-				");
-        }
-        callback(null, file);
-    }
-    return map(checkErrors);
-};
