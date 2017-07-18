85ee8786f897e4652384b26b863f689589a7078c
Joshua Richards <jcrichards1991@live.co.uk>
2017-07-18 15:11:03 +0100

    Moved ApplicationSettings from core to BackofficeAccess

---
+++ src/Our.Shield.BackofficeAccess/Models/ApplicationSettings.cs
@@ -0,0 +1,68 @@
+﻿namespace Our.Shield.BackofficeAccess.Models
+{
+    using System;
+    using System.Configuration;
+    using System.Xml.Linq;
+    using System.Xml.XPath;
+    using Umbraco.Core;
+
+    /// <summary>
+    /// Class for returning values from the app settings section of the web.config
+    /// </summary>
+    internal static class ApplicationSettings
+    {
+        private static string GetAppKeyValue(string key, string fallback = null)
+        {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings[key]))
+            {
+                return fallback;
+            }
+
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        private static string umbracoPath;
+
+        /// <summary>
+        /// Gets the Umbraco Path app setting value
+        /// </summary>
+        public static string UmbracoPath
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(umbracoPath))
+                    return umbracoPath;
+
+                var path = GetAppKeyValue("umbracoPath", "~/umbraco");
+                path = Umbraco.Core.IO.IOHelper.ResolveUrl(path);
+                return umbracoPath = path.EnsureEndsWith('/');
+            }
+        }
+
+        private static string umbracoVersion;
+
+        public static string UmbracoVersion
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(umbracoVersion))
+                    return umbracoVersion;
+
+                return umbracoVersion = GetAppKeyValue("umbracoConfigurationStatus");
+            }
+        }
+
+        public static void SetUmbracoPath(string newUmbracoPath)
+        {
+            ConfigurationManager.AppSettings.Set("umbracoPath", newUmbracoPath);
+                        
+            var path = Umbraco.Core.IO.IOHelper.ResolveUrl(newUmbracoPath);
+            umbracoPath = path.EnsureEndsWith('/');
+        }
+
+        public static void SetUmbracoReservedPaths(string newUmbracoReservedPaths)
+        {
+            ConfigurationManager.AppSettings.Set("umbracoReservedPaths", newUmbracoReservedPaths);
+        }
+    }
+}
