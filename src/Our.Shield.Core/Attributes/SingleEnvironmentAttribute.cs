b0a3fe40462dd75ef18760887416ff6f81dac4a2
Joshua Richards <jcrichards1991@live.co.uk>
2017-08-08 11:57:05 +0100

    Added single environment attr

---
+++ src/Our.Shield.Core/Attributes/SingleEnvironmentAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Our.Shield.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class SingleEnvironmentAttribute : Attribute
+    {
+        
+    }
+}
