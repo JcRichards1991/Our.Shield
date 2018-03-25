e58bacb5d7ee74e06aeabce4b8974e3a0652c4f9
Joshua Richards <jcrichards1991@live.co.uk>
2018-03-25 18:48:36 +0100

    Cherry picked changes needed for umbraco package release into master from release branch

--- src/Our.Shield.Core/Settings/RequestHeaderElement.cs
+++ src/Our.Shield.Core/Settings/RequestHeaderElement.cs
@@ -5,6 +5,10 @@ namespace Our.Shield.Core.Settings
     internal class RequestHeaderElement : ConfigurationElement
     {
         [ConfigurationProperty("header", IsRequired = true, IsKey = true)]
-        public string Header => this["header"] as string;
+        public string Header
+        {
+            get => this["header"] as string;
+            set => this["header"] = value;
+        }
     }
 }
